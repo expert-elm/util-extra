d4d6509b2ef460bea7f49f635f6a2c224e3743f2
zuozijian <zuozijian@bytedance.com>
2019-02-13 19:23:40 +0800

    从函数类型中获取参数类型

--- src/type/Parameter.ts
+++ src/type/Parameter.ts
@@ -1,1 +1,5 @@
-export type Parameter<F> = F
+export type AnyFunction = (...args: any[]) => void
+
+export type Parameters<F extends AnyFunction> = F extends (...args: infer P) => void ? P : never
+
+export type Parameter<F extends AnyFunction, Index extends number> = Parameters<F>[Index]
