448ef7149fce4db1e31e5bdd3c5761e5387bec88
Rabbit <yfhj1990@hotmail.com>
2019-02-19 15:55:23 +0800

    Typo

--- src/type/ParameterType.ts
+++ src/type/ParameterType.ts
@@ -5,7 +5,7 @@ type Parameters<F extends AnyFunction> = F extends (...args: infer P) => void ? 
  * function paramter type
  * 
  * @param F function type
- * @param I argument index
+ * @param I argument index, default to 0
  */
 export type ParameterType<F extends AnyFunction, 
                           I extends number = 0> = Parameters<F>[I]
