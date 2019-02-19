7004edebc10794b5b2aa8ac282f68f6fe170b4b2
Rabbit <yfhj1990@hotmail.com>
2019-02-19 14:25:46 +0800

    Add type.ParamterType default index with 0

---
+++ src/type/ParameterType.ts
@@ -0,0 +1,11 @@
+type AnyFunction = (...args: any[]) => void
+type Parameters<F extends AnyFunction> = F extends (...args: infer P) => void ? P : never
+
+/**
+ * function paramter type
+ * 
+ * @param F function
+ * @param I argument index
+ */
+export type ParameterType<F extends AnyFunction, 
+                          I extends number = 0> = Parameters<F>[I]
