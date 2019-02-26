ae1c7c5a1fc91130c2cbc0225a798cb7608eb6a9
Rabbit <yfhj1990@hotmail.com>
2019-02-26 12:09:59 +0800

    Update container.result

--- src/container/result.test.ts
+++ src/container/result.test.ts
@@ -1,11 +1,15 @@
-import { Ok, Err, Result } from './result'
+import { Ok, Err, isResult } from './result'
 
 const ERR: Error = new Error(`foo`)
 const ERR2: Error = new Error(`bar`)
 
 test(`should be Result`, () => {
-  expect(Ok(42)).toBeInstanceOf(Result)
-  expect(Err(ERR)).toBeInstanceOf(Result)
+  expect(
+    isResult(Ok(42))
+  ).toBe(true)
+  expect(
+    isResult(Err(ERR))
+  ).toBe(true)
 })
 
 test(`should map Result value`, () => {
@@ -16,6 +20,11 @@ test(`should map Result value`, () => {
 test(`should map Result error`, () => {
   expect(Ok(42).mapErr(_ => ERR)).toEqual(Ok(42))
   expect(Err(ERR).mapErr(_ => ERR2)).toEqual(Err(ERR2))
+})
+
+test(`should chain map`, () => {
+  expect(Ok(42).map(_ => 41).map(_ => 40)).toEqual(Ok(40))
+  expect(Ok(42).map(_ => 41).mapErr(_ => ERR)).toEqual(Ok(41))
 })
 
 test(`should unwrap`, () => {
--- src/container/result.ts
+++ src/container/result.ts
@@ -1,63 +1,130 @@
-export class Result<T, E extends Error> {
-  constructor(private value: T | E) {}
+interface Result<T, E> {
+  readonly value: T | E
+  map<U>(fn: (v: T) => U): Result<U, E>
+  mapErr<U extends Error>(fn: (e: E) => U): Result<T, U>
+  isOk(): boolean
+  isErr(): boolean
+  and<U>(res: Result<U, E>): Result<U, E>
+  andThen<U>(op: (v: T) => Result<U, E>): Result<U, E>
+  or<U extends Error>(res: Result<T, U>): Result<T, U>
+  orElse<U extends Error>(op: (e: E) => Result<T, U>): Result<T, U>
+  unwrap(): T
+  unwrapOr<U>(u: U): T | U
+  unwrapOrElse<U>(op: (e: E) => U): T | U
+}
+
+class _Ok<T> implements Result<T, never> {
+  constructor(public readonly value: T) {}
   
-  map<U>(fn: (v: T) => U): Result<U, E> {
-    if(this.value instanceof Error) return new Result(this.value)
-    return new Result(fn(this.value))
+  map<U>(fn: (v: T) => U): Result<U, never> {
+    return new _Ok(fn(this.value))
   }
   
-  mapErr<U extends Error>(fn: (e: E) => U): Result<T, U> {
-    if(!(this.value instanceof Error)) return new Result(this.value)
-    return new Result(fn(this.value))
+  mapErr<U extends Error>(_fn: (e: never) => U): Result<T, never> {
+    return new _Ok(this.value)
   }
 
   isOk(): boolean {
-    return !this.isErr()
-  }
-  isErr(): boolean {
-    return this.value instanceof Error
+    return true
   }
   
-  and<U>(res: Result<U, E>): Result<U, E> {
-    if(this.value instanceof Error) return new Result(this.value)
+  isErr(): boolean {
+    return false
+  }
+  
+  and<U>(res: Result<U, never>): Result<U, never> {
     return res
   }
 
-  andThen<U>(op: (v: T) => Result<U, E>): Result<U, E> {
-    if(this.value instanceof Error) return new Result(this.value)
+  andThen<U>(op: (v: T) => Result<U, never>): Result<U, never> {
     return op(this.value)
   }
 
-  or<U extends Error>(res: Result<T, U>): Result<T, U> {
-    if(!(this.value instanceof Error)) return new Result(this.value)
-    return res
+  or(_res: Result<T, never>): Result<T, never> {
+    return new _Ok(this.value)
   }
 
-  orElse<U extends Error>(op: (v: E) => Result<T, U>): Result<T, U> {
-    if(!(this.value instanceof Error)) return new Result(this.value)
-    return op(this.value)
+  orElse(_op: (e: never) => Result<T, never>): Result<T, never> {
+    return new _Ok(this.value)
   }
 
   unwrap(): T {
-    if(!(this.value instanceof Error)) return this.value
+    return this.value
+  }
+
+  unwrapOr(_v: any): T {
+    return this.value
+  }
+
+  unwrapOrElse<U>(_op: (e: never) => U): T {
+    return this.value
+  }
+}
+
+class _Err<E extends Error> implements Result<never, E> {
+  constructor(public readonly value: E) {}
+  
+  map<U>(_fn: (v: never) => U): Result<never, E> {
+    return new _Err(this.value)
+  }
+  
+  mapErr<U extends Error>(fn: (e: E) => U): Result<never, U> {
+    return new _Err(fn(this.value))
+  }
+
+  isOk(): boolean {
+    return false
+  }
+
+  isErr(): boolean {
+    return true
+  }
+  
+  and(_res: Result<never, E>): Result<never, E> {
+    return new _Err(this.value)
+  }
+
+  andThen(_op: (v: never) => Result<never, E>): Result<never, E> {
+    return new _Err(this.value)
+  }
+
+  or<U extends Error>(res: Result<never, U>): Result<never, U> {
+    return res
+  }
+
+  orElse<U extends Error>(op: (v: E) => Result<never, U>): Result<never, U> {
+    return op(this.value)
+  }
+
+  unwrap(): never {
     throw this.value
   }
 
-  unwrapOr<U>(v: U): U | T {
-    if(this.value instanceof Error) return v
-    return this.value
+  unwrapOr<U>(v: U): U {
+    return v
   }
 
-  unwrapOrElse<U>(op: (e: E) => U): T | U {
-    if(this.value instanceof Error) return op(this.value)
-    return this.value
+  unwrapOrElse<U>(op: (e: E) => U): U {
+    return op(this.value)
   }
 }
 
 export function Ok<T>(v: T): Result<T, never> {
-  return new Result(v)
+  return new _Ok(v)
 }
 
 export function Err<E extends Error>(e: E): Result<never, E> {
-  return new Result(e)
+  return new _Err(e)
+}
+
+export function isResult<T, E>(result: Result<T, E>) {
+  return isOk(result) || isErr(result)
+}
+
+export function isOk<T, E>(result: Result<T, E>) {
+  return result instanceof _Ok
+}
+
+export function isErr<T, E>(result: Result<T, E>) {
+  return result instanceof _Err
 }
