eb6717339225081b348bd650f5d1c50f4aafe83e
Rabbit <yfhj1990@hotmail.com>
2019-03-07 18:38:26 +0800

    Add times docs

--- src/timer/delay.ts
+++ src/timer/delay.ts
@@ -1,6 +1,12 @@
 import sleep from './sleep'
 import { AnyFunction } from '../type/AnyFunction'
 
+/**
+ * delay ms to call function
+ * 
+ * @param ms sleep ms
+ * @param fn caller
+ */
 export default async function delay<F extends AnyFunction>(ms: number, fn: F): Promise<ReturnType<F>> {
   await sleep(ms)
   return fn()
--- src/timer/retry.ts
+++ src/timer/retry.ts
@@ -1,9 +1,13 @@
 import delay from './delay'
 import { AnyFunction } from '../type/AnyFunction'
 
+/** build-in timing function */
 export const enum TimingFunction {
+  /* change by steps */
   Step,
+  /* linear, interval no changed */
   Linear,
+  /* curve, fast forward then slow out */
   Curve
 }
 
@@ -11,6 +15,7 @@ type TimingFunctions = {
   [K in TimingFunction]: TimingFunctionType
 }
 
+/** build-in timing function implements  */
 export const TIMING_FUNCTION: TimingFunctions = {
   [TimingFunction.Step]: a => {
     if(a <= 3) return 0
@@ -21,24 +26,42 @@ export const TIMING_FUNCTION: TimingFunctions = {
   [TimingFunction.Curve]: a => Math.log1p(a)
 }
 
+/** 
+ * timing function type 
+ * 
+ * @param times current run times
+ * @param options retry options
+ */
 export interface TimingFunctionType {
   (times: number, options: Options): number
 }
 
+/** retry options type */
 export interface Options {
+  /** basic interval, default to 1s */
   base: number,
+  /** max retry times, default to 10 times */
   max: number
+  /** timing function, use build-in or custom equation, default to linear */
   func: TimingFunction | TimingFunctionType
 }
 
+/** default retry options */
 export const DEFAULT_OPTIONS: Options = {
   base: 1000,
   max: 10,
   func: TimingFunction.Linear
 }
 
+/** max retry times errors  */
 export const MAX_RETRY_TIMES_ERROR: Error = new Error(`Maximum retry times`)
 
+/**
+ * retry function that not throw
+ *  
+ * @param fn caller
+ * @param options retry options
+ */
 export default async function retry<F extends AnyFunction>(fn: F, options: Partial<Options> = {}): Promise<ReturnType<F>> {
   const opts: Options = { ...DEFAULT_OPTIONS, ...options }
   const { base, max, func } = opts
