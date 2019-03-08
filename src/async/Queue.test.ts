c781ddb6ea746938659731b17253560772ffab74
Rabbit <yfhj1990@hotmail.com>
2019-03-08 10:58:57 +0800

    move sleep to timer.sleep

--- src/async/Queue.test.ts
+++ src/async/Queue.test.ts
@@ -1,5 +1,5 @@
 import { Queue } from './Queue'
-import { sleep } from './sleep'
+import sleep from '../timer/sleep'
 
 test('should queued function be executed serially', async () => {
   let queue = new Queue()
