173b742027fddd6c8a7ad6dc08dbcbbb0ecba4cf
Rabbit <yfhj1990@hotmail.com>
2019-06-22 11:28:00 +0800

    Add date constants

---
+++ src/date/constant.ts
@@ -0,0 +1,12 @@
+/**@inline */export const MILLISECONDS_OF_SECOND: number = 1000
+/**@inline */export const SECONDS_OF_MINUTE: number = 60
+/**@inline */export const MINUTES_OF_HOUR: number = 60
+/**@inline */export const HOURS_OF_DAY: number = 24
+/**@inline */export const MONTHS_OF_YEAR: number = 12
+/**@inline */export const WEEKS_OF_MONTH: number = 4
+/**@inline */export const QUARTERS_OF_YEAR: number = 4
+
+/**@inline */export const MINUTE_SECONDS = MILLISECONDS_OF_SECOND * SECONDS_OF_MINUTE
+/**@inline */export const HOUR_SECONDS = MINUTE_SECONDS * MINUTES_OF_HOUR
+/**@inline */export const DAY_SECONDS = HOUR_SECONDS * HOURS_OF_DAY
+
