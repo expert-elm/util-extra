2515bfc9bfe64a5275beaec4885c990784e29267
Rabbit <yfhj1990@hotmail.com>
2019-06-27 17:28:27 +0800

    Add index generator

--- Foyfile.ts
+++ Foyfile.ts
@@ -1,60 +1,61 @@
 import path from 'path'
 import { sync as glob } from 'glob'
 import { task, fs } from 'foy'
-import * as ts from 'typescript'
+import { Project } from 'ts-morph'
+import repack, { Target } from 'ts-repack'
 
-const OUTPUT_DIRECTORY: string = path.resolve('./dist')
+const OUTPUT_DIRECTORY: string = path.resolve('dist')
+const OUTPUT_MJS_DIRECTORY: string = path.resolve(OUTPUT_DIRECTORY, 'module')
+const OUTPUT_CJS_DIRECTORY: string = path.resolve(OUTPUT_DIRECTORY, '.')
+
 
 task('clean', async () => {
   if(fs.existsSync(OUTPUT_DIRECTORY)) await fs.rmrf(OUTPUT_DIRECTORY)
   await fs.mkdir(OUTPUT_DIRECTORY)
+  await fs.mkdir(OUTPUT_MJS_DIRECTORY)
 })
 
-task('build', ['clean', `build:dist`, `build:deno`], async _ctx => {
-  
+task('build', ['clean'], async ctx => {
+  repack(Target.CJS, { output: OUTPUT_CJS_DIRECTORY })
+  repack(Target.MJS, { output: OUTPUT_MJS_DIRECTORY })
+  ctx.run(`gen:index`)
+  ctx.run(`gen:index:ns`)
 })
 
-task(`build:dist`, async ctx => {
-  await ctx.exec('tsc -p ./tsconfig.build.json')
-  await fs.copy(path.resolve(OUTPUT_DIRECTORY, 'src'), OUTPUT_DIRECTORY)
-  await fs.rmrf(path.resolve(OUTPUT_DIRECTORY, 'src'))
+task('release', [`build`], async ctx => {
+  await ctx.exec('npm version patch')
+  await fs.copy('package.json', path.resolve(OUTPUT_DIRECTORY, 'package.json'))
+  await ctx.cd(OUTPUT_DIRECTORY).exec('npm publish')
 })
 
-task('build:deno', async _ctx => {
-  const DENO_DIRECTORY: string = path.resolve(OUTPUT_DIRECTORY, 'deno')
-  await fs.mkdir(DENO_DIRECTORY)
-  await fs.copy(path.resolve('./src'), DENO_DIRECTORY)
-  
-  glob(path.resolve(DENO_DIRECTORY, '**', '*.ts')).forEach(async p => {
-    if(p.match(/test/)) {
-      await fs.rmrf(p)
-    } else {
-      const source = fs.readFileSync(p, 'utf-8')
-      const resultFile = ts.createSourceFile(
-        p,
-        source,
-        ts.ScriptTarget.Latest,
-        /*setParentNodes*/ false,
-        ts.ScriptKind.TS
-      )
-      const printer = ts.createPrinter()
-      const transformed = ts.transform(resultFile, [ transformer(Extension.TS) ], {
-        module: ts.ModuleKind.ESNext
-      }).transformed
-      
-      const result = printer.printNode(
-        ts.EmitHint.Unspecified,
-        transformed[0],
-        resultFile
-      )
+task(`gen:index:ns`, async () => {
+  const ns = glob(`src/*`).map(dirpath => dirpath.replace(/^src\//, ''))
+  const indexPath: string = path.resolve(OUTPUT_DIRECTORY, `index.js`)
+  const indexContent: string = ns.map(str => `export * from './${str}'`).join('\n')
+  await fs.writeFile(indexPath, indexContent, `utf-8`)
+  console.log(`GENERATE INDEX:\n`, indexContent)
+})
 
-      fs.writeFileSync(p, result, 'utf-8')
-    }
+task(`gen:index`, async () => {
+  const project = new Project({
+    skipFileDependencyResolution: true
   })
-})
+  const ns = glob(`src/*`).map(dirpath => dirpath.replace(/^src\//, ''))
+  ns.forEach(async str => {
+    const indexPath: string = path.resolve(OUTPUT_DIRECTORY, str, `index.js`)
+    const files: string[] = glob(`src/${str}/*.ts`).filter(filepath => !filepath.match(/test/))
+    const indexContent = files.map(file => {
+      const sourceFile = project.addExistingSourceFile(path.resolve(file))
+      const exportSymbols = sourceFile.getExportSymbols().map(sym => sym.compilerSymbol.name).filter(exporter => exporter !== `default`)
+      const defaultExportSymbol = sourceFile.getDefaultExportSymbol()
+      project.removeSourceFile(sourceFile)
+      const filename = path.basename(file, path.extname(file))
+      return `export { ${exportSymbols.join(', ')}${defaultExportSymbol ? `, default as ${filename}` : ``} } from './${filename}'`
+    }).join('\n')
 
-task(`build:browser`, async _ctx => {
-  
+    await fs.writeFile(indexPath, indexContent, `utf-8`)
+    console.log(`GENERATE ${str} INDEX:\n`, indexContent)
+  })
 })
 
 task('test', async ctx => {
@@ -64,33 +65,3 @@ task('test', async ctx => {
 task('test:watch', async ctx => {
   await ctx.exec('jest --watch')
 })
-
-task('release', async ctx => {
-  await ctx.exec('npm version patch')
-  await fs.copy('package.json', path.resolve(OUTPUT_DIRECTORY, 'package.json'))
-  await ctx.cd(OUTPUT_DIRECTORY).exec('npm publish')
-})
-
-enum Extension {
-  TS = '.ts',
-  JS = '.js'
-}
-
-function transformer<T extends ts.Node>(ext: Extension): ts.TransformerFactory<T> {
-  return (context) => {
-    const visitor: ts.Visitor = node => {      
-      if(ts.isSourceFile(node)) return ts.visitEachChild(node, visitor, context)      
-
-      if(ts.isImportDeclaration(node)) {
-        return ts.visitEachChild(node, (node) => {
-          if(!ts.isStringLiteral(node)) return node
-          console.log(node)
-          return ts.createStringLiteral(node.text.replace(/[\'\"]/g, '') + ext)
-        }, context)
-      }
-      
-      return node
-    }
-    return (node) => ts.visitNode(node, visitor)
-  }
-}
