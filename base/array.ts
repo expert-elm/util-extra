61eaece0b476e1d5f0695d8f3e27b2f177dd926f
Rabbit <yfhj1990@hotmali.com>
2021-05-29 00:27:32 +0800

    note:fix ci

--- base/array.ts
+++ base/array.ts
@@ -1,96 +1,189 @@
 /**
- * convert T to T[]
- * @param array array or a element of array
+ * Get array element type
+ * 
+ * @typeParam T - Array type
+ * @example ```ts
+ * ElementType<number[]>      // number
+ * ElementType<Array<string>> // string
+ * ```
  */
-export function ensure_array<T>(array: T | T[]): T[] {
-  return is_array(array) ? array : [ array ]
+export type ElementType<T extends Array<any>> = T extends Array<infer R> ? R : never
+
+
+/**
+ * Ensure value is a array
+ * 
+ * @param value - array or a element of array
+ * @returns array
+ * @example ```ts
+ * ensure_array(42) // [42]
+ * ensure_array([]) // []
+ * ```
+ */
+export function ensure_array<T>(value: T | T[]): T[] {
+  return Array.isArray(value) ? value : [ value ]
 }
 
 /**
- * test value is array
- * @param value value
+ * Test value is array, just wrap `Array.isArray`
+ * 
+ * @param value - test value
+ * @returns test result
+ * @example ```ts
+ * is_array([])        // true
+ * is_array(Array())   // true
+ * is_array({ 0: 42 }) // false
+ * ```
+ * @inline
  */
-export function is_array<T = any>(value: any): value is Array<T> {
+export function is_array(value: any): boolean {
   return Array.isArray(value)
 }
 
 /**
- * test a array have empty element
- * @param array array
+ * Test a array have empty element
+ * 
+ * @typeParam T - array type
+ * @param array - array
+ * @returns test result
+ * @example ```ts
+ * is_empty_array([])      // true
+ * is_empty_array(Array()) // true
+ * ```
  */
 export function is_empty_array<T>(array: Array<T>): boolean {
   return 0 === array.length
 }
 
-//#region choose
-export function choose_array<T>(array: T[], { include = [], exclude = [], equals = Object.is }: ChooseArrayOptions<T> = {}): T[] {
-  const inc: T[] = ensure_array(include)
-  const exc: T[] = ensure_array(exclude)
+/**
+ * Select some elements from array
+ * 
+ * @param array - array
+ * @param options - choose options
+ * @returns choosed array
+ */
+export function select_array<T>(array: T[], options: SelectArrayOptions<T> = {}): T[] {
+  const { 
+    include = [], 
+    exclude = [], 
+    test = Object.is 
+  } = options
+
+  const inc = ensure_array(include)
+  const exc = ensure_array(exclude)
 
   return array
     .concat(inc)
-    .filter(item => !exc.some(e => equals(e, item)))
-}
-
-export interface ChooseArrayOptions<T> {
-  // test: BaseTest<T> | U,
-  readonly include?: T | T[],
-  readonly exclude?: T | T[],
-  readonly equals?: (a: T, b: T) => boolean
-}
-//#endregion
-
-//#region slice_by
-/** string split strategy */
-export const enum SplitStrategy { 
-  /* split by offset length */
-  Length, 
-  /* split by index */
-  Index 
+    .filter(item => !exc.some(e => test(e, item)))
 }
 
 /**
- * split list slices by length or index
+ * Options of choose_array
  * 
- * @param list list
- * @param numbers split by numbers
- * @param strategy how to split string
+ * @typeParam T - array element type
  */
-export function splitSlice(list: string, numbers: number[], strategy?: SplitStrategy): string[]
-export function splitSlice<T>(list: T[], numbers: number[], strategy?: SplitStrategy): T[][]
-export function splitSlice(list: any, numbers: number[], strategy: SplitStrategy = SplitStrategy.Length): any {
-  if('string' === typeof list) return splitString(list, numbers, strategy)
-  return split(list, numbers, strategy)
+export interface SelectArrayOptions<T> {
+  readonly include?: T | T[],
+  readonly exclude?: T | T[],
+  readonly test?: (a: T, b: T) => boolean
 }
 
-function split<T>(list: T[], numbers: number[], strategy: SplitStrategy): T[][] {
-  const func = makeSpliter(strategy)
-  const [ ret, lst ] = numbers.reduce<[T[][], number]>(([ ret, prev ], curr) => {
-    const [ slice, next ] = func(list, prev, curr)
-    ret.push(slice)
-    return [ret, next]
-  }, [[], 0])
+//#region split_array
 
-  ret.push(list.slice(lst))
-  return ret
+/**
+ * cut list by index
+ * 
+ * @param list - list
+ * @param index - list index
+ * @returns splited list
+ * @example ```ts
+ * cut_at([1,2,3], 1) // [[1,2], [3]]
+ * ```
+ */
+export function cut_at(string: string, index: number): [ string, string ]
+export function cut_at<T>(list: T[], index: number): [ T[], T[] ]
+export function cut_at(list: any, index: number): any {
+  if(index < 0) {
+    return [ list.slice(0, index), list.slice(index) ]
+  }
+  else {
+    const pos = index + 1
+    return [ list.slice(0, pos), list.slice(pos) ]
+  }
 }
 
-function splitString(list: string, numbers: number[], strategy: SplitStrategy): string[] {
-  return split(list.split(``), numbers, strategy).map(s => s.join(``))
+/**
+ * cut list by size
+ * 
+ * @param list - list
+ * @param size - size
+ * @returns splited list
+ * @throws size < 0
+ * @example ```ts
+ * split([])         // [[]]
+ * split([1,2,3], 2) // [[1,2], [3]]
+ * ```
+ */
+export function cut(string: string, size: number): [ string, string ]
+export function cut<T>(list: T[], size: number): [ T[], T[] ]
+export function cut(list: any, size: number): any {
+  if(size < 0) throw new Error(`expected size >= 0, got "${size}"`)
+  return [ list.slice(0, size), list.slice(size) ]
 }
 
-function makeSpliter(strategy: SplitStrategy) {
-  return function sliceString<T>(list: T[], prev: number, curr: number): [T[], number] {
-    switch(strategy) {
-      case SplitStrategy.Length: {
-        const next: number = curr + prev
-        return [list.slice(prev, next), next]
+/**
+ * cut list per size
+ * 
+ * @param list - list
+ * @param size - size
+ * @returns splited list
+ * @example ```ts
+ * cut_all([1,2,3,4,5,6], 2)    // [[1,2], [3,4], [5,6]]
+ * cut_all([1,2,3,4,5,6], 1, 2) // [[1], [2,3], [4], [5,6]]
+ * ```
+ */
+export function cut_all(string: string, ...size: number[]): string[] 
+export function cut_all<T>(list: T[], ...size: number[]): T[][] 
+export function cut_all(list: any, ...size: number[]): any {
+  switch(size.length) {
+    case 0: return [list]
+    case 1: return cut_1(list, size[0])
+    default: return cut_n(list, size)
+  }
+
+  function cut_1<T>(list: T[], size: number): T[][] {
+    if(size < 0) throw new Error(`expected size >= 0, got "${size}"`)
+    const arr: T[][] = []
+    const len = list.length
+    let i = 0
+    while(i < len) {
+      const beg = i, end = i + size
+      arr.push(list.slice(beg, end))
+      i = end
+    }
+    return arr
+  }
+
+  function cut_n<T>(list: T[], sizes: number[]): T[][] {
+    const arr: T[][] = []
+    const len = list.length
+    let i = 0, j = 0
+    while(i < len) {
+      const size = sizes[j]
+      if(size < 0) throw new Error(`expected size[${j}] >= 0, got "${size}"`)
+      const beg = i, end = i + size
+      arr.push(list.slice(beg, end))
+      i = end
+      j = next(j)
+    }
+
+    return arr
+
+    function next(index: number) {
+      switch(index) {
+        case size.length - 1: return 0
+        default: return index + 1
       }
-      case SplitStrategy.Index: {
-        const next: number = curr + 1
-        return [list.slice(prev, next), next]
-      }
-      default: throw new Error(`Unknown split strategy "${strategy}"`)
     }
   }
 }
