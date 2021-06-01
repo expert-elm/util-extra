c420fbe2c016b1cafe5f8c4c7e804601881536ea
Rabbit <yfhj1990@hotmali.com>
2021-06-01 09:01:16 +0800

    note:     - add base/function test     - add base/nil test     - remove base/json helers, now only types

--- base/function.ts
+++ base/function.ts
@@ -2,13 +2,23 @@ import { object_type } from "./object"
 
 /**
  * AnyFunction type
- * @typeParam R - return type
+ * 
+ * @typeParam R - function return type
  */
 export type AnyFunction<R = any> = (...args: any[]) => R
 
 /**
- * test value is function
- * @param value value
+ * Test value is function
+ * 
+ * @param value - value
+ * @returns test result
+ * @example ```ts
+ * is_function(function() {})  // true
+ * is_function(function*() {}) // true
+ * is_function(()=>{})         // true
+ * is_function(class {})       // true
+ * ```
+ * @inline
  */
 export function is_function<R>(value: any): value is AnyFunction<R> {
   return 'function' === typeof value || 'Function' === object_type(value)
--- base/nil.ts
+++ base/nil.ts
@@ -3,10 +3,19 @@ import { is_undefined } from './undefined'
 
 export type Nil = undefined | null
 
+/**
+ * Test a value is null or undefined
+ * 
+ * @param value - value
+ * @returns test result
+ * @example ```ts
+ * is_nil(null)      // true
+ * is_nil(undefined) // true
+ * is_nil(0)         // false
+ * is_nil('')        // false
+ * ```
+ * @inline
+ */
 export function is_nil(value: unknown): value is Nil {
   return is_undefined(value) || is_null(value)
-}
-
-export function assert_nil(value: unknown): asserts value is Nil {
-  if(!is_nil(value)) throw new Error(`Uncaught AssertionError: ${value} == undefined or ${value} == null`)
 }
